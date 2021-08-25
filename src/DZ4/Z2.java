138a59a31b1d776b6f5c18892b3fe544134d88c9
SERGIOGIT777 <89037351+SERGIOGIT777@users.noreply.github.com>
2021-08-25 23:20:42 +0300

    DZ4

--- src/DZ4/Z2.java
+++ src/DZ4/Z2.java
@@ -1,22 +1,35 @@
 package DZ4;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Z2 {
     public static void main(String[] args) {
-        var mass = new int[5][5];
+        System.out.println("Введите размер матрицы");
+        var a = new Scanner(System.in).nextInt();
+        var b = new Scanner(System.in).nextInt();
+        var mass = new int[a][b];
         var number = new Random();
         for (int i = 0; i < mass.length; i++) {
             for (int j = 0; j < mass.length; j++) {
-                mass[i][j] = number.nextInt(10);
-                System.out.print(mass[i][j] + " ");
+                mass[i][j] = number.nextInt(50);
+                System.out.printf("%d\t", mass[i][j]);
             }
             System.out.println();
         }
-        System.out.println("Нечетные элементы под главной диагональю:");
+        System.out.println("Нечетные элементы под главной диагональю (включительно):");
         for (int i = 0; i < mass.length; i++) {
             for (int j = 0; j < mass.length; j++) {
                 if (i == j + 1) {
+                    if (mass[i][j] % 2 != 0) {
+                        System.out.print(mass[i][j] + " ");
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < mass.length; i++) {
+            for (int j = 0; j < mass.length; j++) {
+                if (i == j) {
                     if (mass[i][j] % 2 != 0) {
                         System.out.print(mass[i][j] + " ");
                     }
--- src/DZ4/Z3.java
+++ src/DZ4/Z3.java
@@ -1,17 +1,21 @@
 package DZ4;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Z3 {
     public static void main(String[] args) {
-        var mass = new int[5][5];
+        System.out.println("Введите размер матрицы");
+        var a = new Scanner(System.in).nextInt();
+        var b = new Scanner(System.in).nextInt();
+        var mass = new int[a][b];
         var number = new Random();
         var chet = 1;
         var chet1 = 1;
         for (int i = 0; i < mass.length; i++) {
             for (int j = 0; j < mass.length; j++) {
-                mass[i][j] = number.nextInt(10);
-                System.out.print(mass[i][j] + " ");
+                mass[i][j] = number.nextInt(50);
+                System.out.printf("%d\t", mass[i][j]);
                 if (i == j) {
                     chet *= mass[i][j];
                 }
