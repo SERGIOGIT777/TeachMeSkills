b1decc2083899605e73acefb6e5ac32d0222470a
SERGIOGIT777 <89037351+SERGIOGIT777@users.noreply.github.com>
2021-09-12 20:49:31 +0300

    Vizout Test Z4 DZ4

---
+++ src/DZ6/ZD1.java
@@ -0,0 +1,21 @@
+package DZ6;
+
+import java.util.Scanner;
+
+public class ZD1 {
+    public static void main(String[] args) {
+        var scan = new Scanner(System.in).nextLine();
+        try {
+            System.out.println(trim(scan));
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static String trim(String str) throws StringIndexOutOfBoundsException {
+        var str1 = new StringBuffer(str);
+        var s1 = "a";
+        var s2 = "b";
+        return str1.substring(str1.indexOf(s1), str1.lastIndexOf(s2));
+    }
+}
